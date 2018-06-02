afde6b15710056fb86f0d7a1f0a98b36f4c81575
Aardbei <megaloler9000@gmail.com>
2018-06-02 18:08:49 -0500

    cli entry point

---
+++ bot-cli.js
@@ -0,0 +1,13 @@
+/* temporary entry point
+ * interface with the bot from the command line
+ * for testing and stuff
+ */
+
+const cli = new require('./gateways/cli')();
+const shell = require('shell');
+
+cli.onMessage = function(message, attachments) {
+	shell(message, attachments, cli);
+}
+
+cli.engage();
