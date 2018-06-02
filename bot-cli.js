e8a3bcde28f7a098cfbe83606ed9b3fc76fcea63
Aardbei <megaloler9000@gmail.com>
2018-06-02 18:35:07 -0500

    its 'working'

--- bot-cli.js
+++ bot-cli.js
@@ -3,8 +3,8 @@
  * for testing and stuff
  */
 
-const cli = new require('./gateways/cli')();
-const shell = require('shell');
+const cli = new (require('./gateways/cli'))();
+const shell = require('./shell');
 
 cli.onMessage = function(message, attachments) {
 	shell(message, attachments, cli);
--- shell.js
+++ shell.js
@@ -9,30 +9,19 @@ const config = require("./config");
 
 /* BOT COMMAND FUNCTIONS */
 
-// stop playing in the channel of the guild the message is from
-function stopPlayingTune(arg, args, message)
-{
-	if(message.guild && stopSound(message.guild))
-	{
-		sendBotString("onStopTune", (msg) => reply(message, msg));
-		voiceEvent(message.guild);
-	}
-	else sendBotString("onNotPlayingTune", (msg) => reply(message, msg));
-}
-
 // respond to the message with examples
-function requestExamples(arg, args, message)
+function requestExamples(arg, args, gateway)
 {
 	// get the list of example strings
 	const ls = Object.keys(config.examples).map((key) => {
 		const example = config.examples[key];
 		return `**${key}**:${ example.credit ? ` _(sequenced by ${example.credit})_` : " " }\`\`\`${config.trigger}${example.example}\`\`\``;
 	});
-	sendBotString("onExampleRequest", (msg) => message.author.send(msg), (msg) => message.author.send(msg), `\n\n${ls.join("\n\n")}`, "\n\n");
+	gateway.send(`${config.botStrings['onExampleRequest'].string}\n\n${ls.join("\n\n")}`);
 }
 
 // see what known instruments there are
-function requestInstruments(arg, args, message)
+function requestInstruments(arg, args, gateway)
 {
         // get the list of instrument strings
         const ls = Array(128).fill().map((v, i) => {
@@ -43,47 +32,47 @@ function requestInstruments(arg, args, message)
                 }).join(" ");
                 return `• \`p${parseInt(i) + 1}\`\t${aliases}`;
         });
-        sendBotString("onInstrumentRequest", (msg) => message.author.send(msg), (msg) => message.author.send(msg), `\n${ls.join("\n")}`);
+	gateway.send(`${config.botStrings['onInstrumentRequest'].string}\n${ls.join("\n")}`);
 }
 
 // respond with help message
-function requestHelp(arg, args, message)
+function requestHelp(arg, args, gateway)
 {
-	sendBotString("onHelpRequest", (msg) => reply(message, msg), (msg) => message.channel.send(msg));
+	gateway.send(config.botStrings['onHelpRequest'].string);
 }
 
 // respond with tutorial message
-function requestTutorial(arg, args, message)
+function requestTutorial(arg, args, gateway)
 {
-	sendBotString("onTutorialRequest", (msg) => message.author.send(msg), (msg) => message.author.send(msg));
+	gateway.send(config.botStrings['onTutorialRequest'].string);
 }
 
 // respond with discord server invite link in private messages
-function requestInviteLink(arg, args, message)
+function requestInviteLink(arg, args, gateway)
 {
-	sendBotString("onInviteLinkRequest", (msg) => message.author.send(msg));
+	gateway.send(config.botStrings['onInviteLinkRequest'].string);
 }
 
 // respond with github link in private messages
-function requestGithubLink(arg, args, message)
+function requestGithubLink(arg, args, gateway)
 {
-	sendBotString("onGithubLinkRequest", (msg) => message.author.send(msg));
+	gateway.send(config.botStrings['onGithubLinkRequest'].string);
 }
 
 // respond with support invite
-function requestSupport(arg, args, message)
+function requestSupport(arg, args, gateway)
 {
-	sendBotString("onSupportRequest", (msg) => message.author.send(msg));
+	gateway.send(config.botStrings['onSupportRequest'].string);
 }
 
 // respond with info message
-function requestInfo(arg, args, message)
+function requestInfo(arg, args, gateway)
 {
-	sendBotString("onInfoRequest", (msg) => reply(message, msg), (msg) => message.channel.send(msg));
+	gateway.send(config.botStrings['onInfoRequest'].string);
 }
 
 // respond with list of commands
-function requestCommandListing(arg, args, message)
+function requestCommandListing(arg, args, gateway)
 {
         // get the list of command strings
         const ls = Object.keys(config.commands).sort().map((key) => {
@@ -93,7 +82,7 @@ function requestCommandListing(arg, args, message)
 		const aliasString = aliases.join(", ");
 		return `• ${aliasString}:\n\t\t**->** ${description}`;
 	});
-        sendBotString("onCommandListingRequest", (msg) => message.author.send(msg), (msg) => message.author.send(msg), `\n${ls.join("\n")}`);
+	gateway.send(`${config.botStrings['onCommandListingRequest'].string}\n${ls.join("\n")}`);
 }
 
 // map of commands
@@ -110,16 +99,14 @@ function registerCommand(names, f)
 }
 
 // register the commands
-registerCommand(config.commands.joinVoiceChannel.aliases, joinVoiceChannel);
-registerCommand(config.commands.leaveVoiceChannel.aliases, leaveVoiceChannel);
-registerCommand(config.commands.autoCommand.aliases, autoCommand);
-registerCommand(config.commands.requestSheets.aliases, requestSheets);
+/*registerCommand(config.commands.requestSheets.aliases, requestSheets);
 registerCommand(config.commands.requestMidiFile.aliases, requestMidiFile);
 registerCommand(config.commands.requestLilyPondFile.aliases, requestLilyPondFile);
 registerCommand(config.commands.requestPdfFile.aliases, requestPdfFile);
 registerCommand(config.commands.playTune.aliases, playTune);
 registerCommand(config.commands.repeatTune.aliases, repeatTune);
 registerCommand(config.commands.stopPlayingTune.aliases, stopPlayingTune);
+*/
 registerCommand(config.commands.requestHelp.aliases, requestHelp);
 registerCommand(config.commands.requestTutorial.aliases, requestTutorial);
 registerCommand(config.commands.requestInstruments.aliases, requestInstruments);
@@ -130,23 +117,24 @@ registerCommand(config.commands.requestInfo.aliases, requestInfo);
 registerCommand(config.commands.requestSupport.aliases, requestSupport);
 registerCommand(config.commands.requestCommandListing.aliases, requestCommandListing);
 
+
 /* MAIN BOT INTERFACE */
 
 // executes a command
 // given cmd name, arg string, args list, and originating discord message
-function executeCommand(cmd, arg, args, message)
+function executeCommand(cmd, arg, args, gateway)
 {
 	// get the command function and call it
 	// return false if command doesn't exist
 	const command = commands[cmd];
-	if(command) command(arg, args, message);
+	if(command) command(arg, args, gateway);
 	else return false;
 	return true;
 }
 
 // process a message to the bot
 // the message string, and the originating discord message object
-function processBotMessage(msg, message)
+function processBotMessage(msg, attachments, gateway)
 {
 	// cmd is case insensitive, args retain case
 	const words = msg.split(/\s+/g);
@@ -159,73 +147,8 @@ function processBotMessage(msg, message)
 
 	// execute the command
 	// assume "auto" command if none other found
-	if(!executeCommand(cmd, arg, args, message))
-		commands.auto(msg, words, message);
+	if(!executeCommand(cmd, arg, args, gateway))
+		gateway.send("cmd not found lol");
 }
 
-// remove mentions of the bot from a message
-function removeMentions(msg)
-{
-	const mention = `<@${client.user.id}>`;
-	return msg.replace(new RegExp(mention, "g"), "");
-}
-
-// on message recieve
-// the message (string), any attachments, and the source gateway
-function handleMessage(message, attachments, gateway) => {
-	// ignore messages from self
-	if(client.user.id === message.author.id) return;
-	// ignore messages from bots
-	if(message.author.bot) return;
-
-	// figure out if a message is directed at the bot or not
-	// and extract the intended message to the bot
-        const content = message.content.trim();
-	const attachment = message.attachments.first();
-        const dm = !message.guild;
-	// is it safe to use arbitrary string as regex ? ?
-	const triggerCount = (content.match(new RegExp(config.trigger, "g")) || []).length;
-        const triggered = content.startsWith(config.trigger) && (!config.singleTrigger || triggerCount == 1);
-	const mentioned = message.mentions.users.has(client.user.id);
-	const inBotChannel = config.enableBotChannelAddressing && message.channel.name === config.botChannel;
-	const blockCodeHead = "```";
-	const blockCodeHeadLily = `\`\`\`${config.blockCodeAlias}\n`;
-	const blockCode = content.indexOf(blockCodeHead) != -1;
-	const blockCodeLily = content.indexOf(blockCodeHeadLily) != -1;
-	const clean = mentioned ? removeMentions(content).trim() : content;
-	// i don't like that this is var and not const
-	var msg = triggered ? clean.slice(config.trigger.length).trim() : clean;
-
-	// care only if it isn't empty or has an attachment
-	// that and must be addressing the bot in some way
-	if((msg.length || attachment) && (dm || triggered || mentioned || inBotChannel || blockCodeLily))
-	{
-		// first check to see if it was addressed by code block
-		// if so, extract the intended command from the message
-		if(blockCode)
-		{
-			// if it was given code blocks, the first block is the arg string
-			// and the command is the first word (if any)
-			// i find this ugly parsing, allowing for unclosed blocks and all, w/e
-			const parts = msg.split(blockCodeLily ? blockCodeHeadLily : blockCodeHead);
-			const words = parts[0].split(/\s+/g);
-			const cmd = words[0].toLowerCase();
-			const arg = parts[1].split("```")[0].trim();
-			// reform the msg from the cmd and the arg from the code block
-			// only include cmd if it is a known command
-			msg = (commands[cmd] ? [cmd] : []).concat([arg]).join(" ");
-		}
-
-		// log received message directed at the bot
-		const loggedMsg = msg.split("\n").map((line, i) => {
-			return i ? `\t${line}` : line;
-		}).join("\n");
-		if(dm) console.log(`${message.author.tag}> ${loggedMsg}`);
-		else console.log(`${message.guild.name}> #${message.channel.name}> ${message.author.tag}> ${loggedMsg}`);
-
-		// go handle the message to the bot
-		processBotMessage(msg, message);
-	}
-});
-
-module.exports = handleMessage;
+module.exports = processBotMessage;
